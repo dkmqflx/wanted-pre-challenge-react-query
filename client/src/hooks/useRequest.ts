82175dd334b261aa94ed711a00767beba0429c78
Dohyun Kim <dkmqflx@gmail.com>
2022-12-13 04:45:51 +0900

    feat: add request hook

--- client/src/hooks/useRequest.ts
+++ client/src/hooks/useRequest.ts
@@ -1,8 +1,21 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery, QueryKey } from '@tanstack/react-query';
+
+type requestOptionType<TData = unknown, TError = unknown> = {
+  onSuccess?: (data: TData) => void;
+  onError?: (error: TError) => void;
+};
 
 type mutateOptionType<TData = unknown, TError = unknown> = {
   onSuccess?: (data: TData) => void;
   onError?: (error: TError) => void;
+};
+
+export const useRequest = <TError = unknown, TData = unknown>(
+  key: QueryKey,
+  request: () => TData | Promise<TData>,
+  option?: requestOptionType<TData, TError>
+) => {
+  return useQuery(key, request, { ...option });
 };
 
 export const useMutate = <

6695e1560ccca549a6ae909d3e3a1aad491fc4ec
Dohyun Kim <dkmqflx@gmail.com>
2022-12-14 15:24:09 +0900

    refacotr(): useRequest generic types

--- client/src/hooks/useRequest.ts
+++ client/src/hooks/useRequest.ts
@@ -1,21 +1,17 @@
 import { useMutation, useQuery, QueryKey } from '@tanstack/react-query';
 
-type requestOptionType<TData = unknown, TError = unknown> = {
+type requestOptionType<TData, TError> = {
   onSuccess?: (data: TData) => void;
   onError?: (error: TError) => void;
   enabled?: boolean;
 };
 
-type mutateOptionType<
-  TData = unknown,
-  TError = unknown,
-  TVariables = unknown
-> = {
+type mutateOptionType<TData, TError, TVariables> = {
   onSuccess?: (data: TData, variables?: TVariables) => void;
   onError?: (error: TError) => void;
 };
 
-export const useRequest = <TError = unknown, TData = unknown>(
+export const useRequest = <TError, TData>(
   key: QueryKey,
   request: () => TData | Promise<TData>,
   option?: requestOptionType<TData, TError>
@@ -23,11 +19,7 @@ export const useRequest = <TError = unknown, TData = unknown>(
   return useQuery(key, request, { ...option });
 };
 
-export const useMutate = <
-  TData = unknown,
-  TError = unknown,
-  TVariables = unknown
->(
+export const useMutate = <TData, TError, TVariables>(
   mutationFn: (data: TVariables) => Promise<TData>,
   option?: mutateOptionType<TData, TError, TVariables>
 ) => {
